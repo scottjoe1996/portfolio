3e7c4f259c6aa4a26db7c8ae98208daaf52476bc
Joseph Scott <scottjoe1996@yahoo.co.uk>
2025-06-21 09:51:11 +0100

    styled dropdown nav button component

--- components/icons/hamburger-menu/hamburger-menu.tsx
+++ components/icons/hamburger-menu/hamburger-menu.tsx
@@ -6,7 +6,7 @@ interface HamburgerMenuProps {
 
 const HamburgerMenu: React.FC<HamburgerMenuProps> = ({ size }) => (
   <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 170.08 184.25' height={size}>
-    <g id='hamburger-menu'>
+    <g id='hamburger-menu' fill='white'>
       <g id='bottom-bar'>
         <rect x='0' y='0' width='170.08' height='42.52' rx='21.26' ry='21.26' />
       </g>
--- components/navbar/drop-down-nav-button.tsx
+++ components/navbar/drop-down-nav-button.tsx
@@ -3,10 +3,12 @@ import HamburgerMenu from '../icons/hamburger-menu/hamburger-menu';
 
 const DropDownNavButton: React.FC<React.PropsWithChildren> = ({ children }) => {
   return (
-    <nav>
-      <HamburgerMenu size='2rem' />
-      {children}
-    </nav>
+    <div className='flex justify-end pr-2'>
+      <nav className='ml-auto bg-gray-800 p-2 rounded-b-md shadow-xl'>
+        <HamburgerMenu size='2rem' />
+        {children}
+      </nav>
+    </div>
   );
 };
 
