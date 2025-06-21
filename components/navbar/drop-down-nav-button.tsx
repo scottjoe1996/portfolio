28f73882aa8381ac45505e4b2c08e548858d585b
Joseph Scott <scottjoe1996@yahoo.co.uk>
2025-06-21 10:13:17 +0100

    dropdown nav button appears after scrolling passed the navbar

--- components/navbar/drop-down-nav-button.tsx
+++ components/navbar/drop-down-nav-button.tsx
@@ -3,12 +3,10 @@ import HamburgerMenu from '../icons/hamburger-menu/hamburger-menu';
 
 const DropDownNavButton: React.FC<React.PropsWithChildren> = ({ children }) => {
   return (
-    <div className='flex justify-end pr-2'>
-      <nav className='ml-auto bg-gray-800 p-2 rounded-b-md shadow-xl'>
-        <HamburgerMenu size='2rem' />
-        {children}
-      </nav>
-    </div>
+    <nav className='fixed top-0 right-2 ml-auto bg-gray-800 p-2 rounded-b-md shadow-xl'>
+      <HamburgerMenu size='2rem' />
+      {children}
+    </nav>
   );
 };
 
--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -1,11 +1,32 @@
+'use client';
+
 import React from 'react';
 import DropDownNavButton from './drop-down-nav-button';
 
 const Navbar: React.FC<React.PropsWithChildren> = ({ children }) => {
+  const navBarRef = React.useRef<HTMLElement>(null);
+
+  const [showNavDropDown, setShowNavDropDown] = React.useState(false);
+
+  const onScroll = React.useCallback(() => {
+    if (navBarRef.current?.clientHeight && window.scrollY > navBarRef.current.clientHeight) {
+      setShowNavDropDown(true);
+    } else {
+      setShowNavDropDown(false);
+    }
+  }, []);
+
+  React.useEffect(() => {
+    window.addEventListener('scroll', onScroll);
+    return () => window.removeEventListener('scroll', onScroll);
+  }, [onScroll]);
+
   return (
     <>
-      <nav className='bg-gray-800 flex justify-center items-center p-4 w-full shadow-xl gap-10'>{children}</nav>
-      <DropDownNavButton />
+      <nav ref={navBarRef} className='bg-gray-800 flex justify-center items-center p-4 w-full shadow-xl gap-10'>
+        {children}
+      </nav>
+      {showNavDropDown && <DropDownNavButton />}
     </>
   );
 };
