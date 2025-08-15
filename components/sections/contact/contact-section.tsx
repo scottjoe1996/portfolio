3e11d5672bf2c8a86b74215d89df7db78c331002
Joseph Scott <scottjoe1996@yahoo.co.uk>
2025-08-15 15:37:17 +0100

    added error logic in contact section

--- components/sections/contact/contact-section.tsx
+++ components/sections/contact/contact-section.tsx
@@ -6,22 +6,31 @@ import sendMessage from "@/actions/message";
 
 import ContactForm from "./contact-form";
 
+type FormState = "INITIAL" | "SENT" | "ERROR";
+
 const ContactSection: React.FC = () => {
-  const [hasSentMail, setHasSentMail] = React.useState(false);
+  const [formState, setFormState] = React.useState<FormState>("INITIAL");
 
   return (
     <section
       id="contact"
       className="flex w-full items-center justify-center rounded-t-4xl bg-zinc-200 p-4"
     >
-      <div className="w-full max-w-2xl">
+      <div className="w-full max-w-4xl">
         <h2>Contact me</h2>
         <div className="flex min-h-[440px] flex-col items-center justify-center gap-2">
-          {hasSentMail ? (
+          {formState === "SENT" ? (
             <>
               <p className="text-5xl font-bold">Email recieved!</p>
               <p>
                 Thanks for reaching out, I'll get back to you as soon as I can!
+              </p>
+            </>
+          ) : formState === "ERROR" ? (
+            <>
+              <p className="text-5xl font-bold">Error!</p>
+              <p>
+                There was an error sending your message. Please try again later.
               </p>
             </>
           ) : (
@@ -29,7 +38,9 @@ const ContactSection: React.FC = () => {
               onSendMessage={(name, email, message) =>
                 sendMessage(name, email, message).then((response) => {
                   if (response.success) {
-                    setHasSentMail(true);
+                    setFormState("SENT");
+                  } else {
+                    setFormState("ERROR");
                   }
                 })
               }
