95f5d94f1a02aeeaa9d5b78f21cff2c17337a378
Joseph Scott <scottjoe1996@yahoo.co.uk>
2025-08-18 15:46:38 +0100

    exhanced experience cards to take in skillsGained array

--- components/sections/experience/experience-card.tsx
+++ components/sections/experience/experience-card.tsx
@@ -3,16 +3,28 @@ import React from "react";
 interface ExperienceCardProps {
   role: string;
   description: string;
+  skillsGained: string[];
 }
 
 const ExperienceCard: React.FC<ExperienceCardProps> = ({
   role,
   description,
+  skillsGained,
 }) => {
   return (
-    <div className="bg-foreground w-full rounded-md p-3 text-zinc-50 shadow-md shadow-zinc-600/30">
+    <div className="bg-foreground flex-[1] rounded-md p-3 text-zinc-50 shadow-md shadow-zinc-600/30">
       <p>{role}</p>
-      <p className="text-sm text-zinc-400">{description}</p>
+      <p className="mb-2 text-sm text-zinc-400">{description}</p>
+      <div className="flex gap-2">
+        {skillsGained.map((skill) => (
+          <div
+            key={skill}
+            className="border-primary rounded-2xl border-2 bg-emerald-950 px-2 py-1 text-sm font-semibold text-nowrap"
+          >
+            {skill}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
--- components/sections/experience/experience-section.tsx
+++ components/sections/experience/experience-section.tsx
@@ -6,14 +6,16 @@ const ExperienceSection: React.FC = () => {
   return (
     <section id="experience" className="mb-4 w-full max-w-4xl">
       <SectionHeading title="Experience" />
-      <div className="flex justify-between gap-2">
+      <div className="flex flex-wrap justify-between gap-2">
         <ExperienceCard
           role="Fullstack Software Consultant"
           description="Wow would you look at that I've got experience!"
+          skillsGained={["Typescript", "AWS", "Angular", "Java"]}
         />
         <ExperienceCard
           role="Senior Software Engineer"
           description="Wow would you look at that I've got experience!"
+          skillsGained={["Typescript", "AWS", "React", "Material UI"]}
         />
       </div>
     </section>
