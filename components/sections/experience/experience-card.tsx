2860fbdead2783ef09aba5988d91785f135d60eb
Joseph Scott <scottjoe1996@yahoo.co.uk>
2025-08-21 00:08:58 +0100

    added duration to experience cards

--- components/sections/experience/experience-card.tsx
+++ components/sections/experience/experience-card.tsx
@@ -1,29 +1,39 @@
 import React from "react";
 
+interface Duration {
+  startYear: number;
+  endYear?: number;
+}
+
 interface ExperienceCardProps {
   role: string;
   description: string;
   skillsGained: string[];
+  duration: Duration;
 }
 
 const ExperienceCard: React.FC<ExperienceCardProps> = ({
   role,
   description,
   skillsGained,
+  duration,
 }) => {
   return (
-    <div className="bg-foreground flex-[1] rounded-md p-3 text-zinc-50 shadow-md shadow-zinc-600/30">
-      <p>{role}</p>
-      <p className="mb-2 text-sm text-zinc-400">{description}</p>
-      <div className="flex gap-2">
-        {skillsGained.map((skill) => (
-          <div
-            key={skill}
-            className="border-primary rounded-2xl border-2 bg-emerald-950 px-2 py-1 text-sm font-semibold text-nowrap"
-          >
-            {skill}
-          </div>
-        ))}
+    <div className="bg-foreground flex flex-[1] flex-row gap-2 rounded-md p-3 text-zinc-50 shadow-md shadow-zinc-600/30">
+      <p className="flex-1/3 text-sm leading-6 font-semibold text-nowrap">{`${duration.startYear} — ${duration.endYear ?? "PRESENT"}`}</p>
+      <div className="flex-2/3">
+        <p>{role}</p>
+        <p className="mb-2 text-sm text-zinc-400">{description}</p>
+        <div className="flex gap-2">
+          {skillsGained.map((skill) => (
+            <div
+              key={skill}
+              className="border-primary rounded-2xl border-2 bg-emerald-950 px-2 py-1 text-sm font-semibold text-nowrap"
+            >
+              {skill}
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
--- components/sections/experience/experience-section.tsx
+++ components/sections/experience/experience-section.tsx
@@ -4,18 +4,20 @@ import ExperienceCard from "./experience-card";
 
 const ExperienceSection: React.FC = () => {
   return (
-    <section id="experience" className="mb-4 w-full max-w-4xl">
+    <section id="experience" className="mb-4 w-full max-w-7xl">
       <SectionHeading title="Experience" />
       <div className="flex flex-wrap justify-between gap-2">
-        <ExperienceCard
-          role="Fullstack Software Consultant"
-          description="Wow would you look at that I've got experience!"
-          skillsGained={["Typescript", "AWS", "Angular", "Java"]}
-        />
         <ExperienceCard
           role="Senior Software Engineer"
           description="Wow would you look at that I've got experience!"
           skillsGained={["Typescript", "AWS", "React", "Material UI"]}
+          duration={{ startYear: 2020 }}
+        />
+        <ExperienceCard
+          role="Fullstack Software Consultant"
+          description="Wow would you look at that I've got experience!"
+          skillsGained={["Typescript", "AWS", "Angular", "Java"]}
+          duration={{ startYear: 2018, endYear: 2019 }}
         />
       </div>
     </section>
