a67f5b041dbb01eb1f7ba563a0edcd0d8dc176df
Joseph Scott <scottjoe1996@yahoo.co.uk>
2025-08-24 09:39:19 +0100

    created extra small breakpoint and refactored experience card to work at that breakpoint

--- components/sections/experience/experience-card.tsx
+++ components/sections/experience/experience-card.tsx
@@ -14,13 +14,20 @@ const ExperienceCard: React.FC<ExperienceCardProps> = ({
   skillsGained,
   duration,
 }) => {
+  const durationText = createDurationText(duration);
+
   return (
     <div className="bg-foreground flex flex-1 flex-row gap-4 rounded-md p-3 text-zinc-50 shadow-md shadow-zinc-600/30">
-      <span className="inline w-30 text-sm leading-6 font-semibold text-nowrap md:hidden">
-        {createDurationText(duration)}
+      <span className="xs:inline hidden w-30 text-sm leading-6 font-semibold text-nowrap text-zinc-300 md:hidden">
+        {durationText}
       </span>
       <div>
-        <p className="mb-2">{role}</p>
+        <div className="mb-2">
+          <p className="xs:hidden inline text-sm font-semibold text-zinc-300">
+            {durationText}
+          </p>
+          <p>{role}</p>
+        </div>
         <p className="mb-2 text-sm text-zinc-400">{description}</p>
         <div className="flex flex-wrap gap-2">
           {skillsGained.map((skill) => (
