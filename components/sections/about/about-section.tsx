f1c204222c168499f9745d256752dec1cc652255
Joseph Scott <scottjoe1996@yahoo.co.uk>
2025-08-27 17:36:01 +0100

    added text color to section wrapper and initial about text

--- components/sections/about/about-section.tsx
+++ components/sections/about/about-section.tsx
@@ -7,8 +7,36 @@ const AboutSection: React.FC = () => {
     <SectionWrapper
       id="about"
       title="About Me"
-      bgColor="bg-zinc-700"
-    ></SectionWrapper>
+      bgColor="bg-foreground"
+      textColor="text-zinc-50"
+    >
+      <div className="flex justify-between">
+        <div className="w-full">
+          <p className="mb-6">
+            I'm a passionate full stack developer with an enthusiasm for
+            crafting <b>exceptional digital experiences.</b> I have professional
+            experience working with Java, TypeScript, React and Angular; all
+            being used in tandem with AWS to build scalable and secure cloud
+            solutions.
+          </p>
+          <p>
+            Dedicated to creating intuitive and visually appealing user
+            interfaces. My UX design skills allow me to bridge the gap between
+            functionality and aesthetics. My design decisions are always
+            <b>powered by data</b> received through thorough user research,
+            ensuring that the resulting UI provides a delightful user
+            experience.
+          </p>
+          <p className="mt-6">
+            In an ever-evolving industry, I believe in the power of
+            collaboration and continuous learning. Together, we can create
+            solutions that make a positive impact on the world and push the
+            boundaries of what's possible in the digital realm.
+          </p>
+        </div>
+        <div className="h-50 w-full bg-amber-500">TODO</div>
+      </div>
+    </SectionWrapper>
   );
 };
 
--- components/sections/section-wrapper.tsx
+++ components/sections/section-wrapper.tsx
@@ -5,18 +5,20 @@ interface SectionWrapperProps extends React.PropsWithChildren {
   id: string;
   title: string;
   bgColor?: string;
+  textColor?: string;
 }
 
 const SectionWrapper: React.FC<SectionWrapperProps> = ({
   id,
   title,
   bgColor,
+  textColor,
   children,
 }) => {
   return (
     <section
       id={id}
-      className={`flex w-full items-center justify-center p-4 ${bgColor}`}
+      className={`flex w-full items-center justify-center p-4 ${bgColor} ${textColor}`}
     >
       <div className="mb-4 w-full max-w-7xl">
         <SectionHeading title={title} />
