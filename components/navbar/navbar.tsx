c9f9ff3df562d7cae6cd107d2632098b78f5d8c1
Joseph Scott <scottjoe1996@yahoo.co.uk>
2025-09-07 19:33:56 +0100

    added hamburger menu on small screens

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -3,6 +3,7 @@
 import React from "react";
 import DropDownNavButton from "./drop-down-nav-button";
 import RetroComputer from "../icons/retro-computer";
+import HamburgerMenu from "../icons/hamburger-menu";
 
 const Navbar: React.FC<React.PropsWithChildren> = ({ children }) => {
   const navBarRef = React.useRef<HTMLElement>(null);
@@ -33,12 +34,15 @@ const Navbar: React.FC<React.PropsWithChildren> = ({ children }) => {
         className="container-shadow bg-foreground m-4 box-border flex items-center justify-end rounded-md px-8 py-3 sm:gap-16"
         inert={showNavDropDown}
       >
-        <div className="mr-auto -ml-3 text-white">
-          <RetroComputer size="3.5rem" />
+        <div className="mr-auto -ml-4.5 text-white">
+          <RetroComputer size="3rem" />
         </div>
         {React.Children.map(children, (child) => (
           <div className="mt-1 hidden sm:inline-block">{child}</div>
         ))}
+        <div className="-mr-3 sm:hidden">
+          <HamburgerMenu size="2.25rem" />
+        </div>
       </nav>
       <DropDownNavButton show={showNavDropDown}>{children}</DropDownNavButton>
     </>
