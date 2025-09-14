ef14a9ee431602eeacc6a8e9f7a8419955160770
Joseph Scott <scottjoe1996@yahoo.co.uk>
2025-09-14 22:35:47 +0100

    no longer need to use useLayoutEffect in text drop

--- components/sections/home/text-drop.tsx
+++ components/sections/home/text-drop.tsx
@@ -7,49 +7,17 @@ interface TextDropProps {
   className?: string;
 }
 
-const ID_PREFIX = "drop-text";
-
 const TextDrop: React.FC<TextDropProps> = ({ texts, className }) => {
-  const [containerWidth, setContainerWidth] = React.useState(0);
-  const [containerHeight, setContainerHeight] = React.useState(0);
-
-  React.useLayoutEffect(() => {
-    let largestWidth = 0;
-    let largestHeight = 0;
-
-    texts.forEach((_text, index) => {
-      const element = document.getElementById(`${ID_PREFIX}-${index}`);
-
-      if (element) {
-        if (element.offsetWidth > largestWidth) {
-          largestWidth = element.offsetWidth;
-        }
-        if (element.offsetHeight > largestHeight) {
-          largestHeight = element.offsetHeight;
-        }
-      }
-    });
-    setContainerWidth(largestWidth);
-    setContainerHeight(largestHeight);
-  }, [texts]);
-
   return (
-    <span
-      className={`${className} relative inline-grid`}
-      style={{ width: containerWidth, height: containerHeight }}
-    >
-      {texts.map((text, index) => {
-        const id = `${ID_PREFIX}-${index}`;
-        return (
-          <motion.span
-            id={id}
-            key={id}
-            className="col-start-1 row-start-1 text-nowrap"
-          >
-            &nbsp;{text}
-          </motion.span>
-        );
-      })}
+    <span className={`${className} relative inline-grid`}>
+      {texts.map((text, index) => (
+        <motion.span
+          key={index}
+          className="col-start-1 row-start-1 text-nowrap"
+        >
+          &nbsp;{text}
+        </motion.span>
+      ))}
     </span>
   );
 };
