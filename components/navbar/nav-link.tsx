fbba63410f931e7eedf5752cc98eee65c1173a1a
Joseph Scott <scottjoe1996@yahoo.co.uk>
2025-10-14 00:03:48 +0100

    stop event propagation with navlink

--- components/navbar/nav-link.tsx
+++ components/navbar/nav-link.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Link from "next/link";
 import React from "react";
 
@@ -9,8 +11,19 @@ const NavLink: React.FC<React.PropsWithChildren<NavLinkProps>> = ({
   children,
   href,
 }) => {
+  const disablePropagation = React.useCallback(
+    (event: React.MouseEvent<HTMLAnchorElement>) => {
+      event.stopPropagation();
+    },
+    [],
+  );
+
   return (
-    <Link href={href} className="group font-bold text-zinc-100 outline-none">
+    <Link
+      href={href}
+      className="group font-bold text-zinc-100 outline-none"
+      onClick={disablePropagation}
+    >
       <div className="w-fit">
         {children}
         <div className="h-1 w-0 bg-zinc-100 transition-[width] duration-300 ease-in-out group-hover:w-full group-focus:w-full" />
